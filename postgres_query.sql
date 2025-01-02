c23684c9feeab7f534f07f3031b518885cd055e4
Manvith Katkuri <manvithkatkuri000@gmail.com>
2025-01-02 01:27:21 -0500

    Add files via upload

---
+++ postgres_query.sql
@@ -0,0 +1,5 @@
+INSERT INTO pageview_counts VALUES ('Facebook', 385, '2025-01-02 06:08:22.730546+00:00');
+INSERT INTO pageview_counts VALUES ('Google', 274, '2025-01-02 06:08:22.730546+00:00');
+INSERT INTO pageview_counts VALUES ('Microsoft', 128, '2025-01-02 06:08:22.730546+00:00');
+INSERT INTO pageview_counts VALUES ('Apple', 20, '2025-01-02 06:08:22.730546+00:00');
+INSERT INTO pageview_counts VALUES ('Amazon', 3, '2025-01-02 06:08:22.730546+00:00');
